2013eb301bde2d80c08c26de7c36f910d5ac1843
Euan Goodbrand <70519638+euangoodbrand@users.noreply.github.com>
2021-11-14 20:55:26 +0000

    Added Guest GUI with GuestAccount Page
    Added Guest GUI with GuestAccount Page

---
+++ src/GuestGUI/GuestAccount.java
@@ -0,0 +1,104 @@
+
+package GuestGUI;
+
+import java.awt.EventQueue;
+import javax.swing.*;
+
+
+import Controller.Controller;
+import GUI.Login;
+import GUI.MainModule;
+import GUI.NavEnquirer;
+import HostGUI.NavHost;
+import GUI.MainModule.EDITPROPERTY;
+import GUI.MainModule.STATE;
+import GUI.MainModule.USER;
+import Model.Model;
+
+import java.awt.SystemColor;
+import java.awt.Toolkit;
+import java.awt.Color;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.ActionEvent;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.Font;
+
+public class GuestAccount extends JFrame{
+
+
+	private NavHost navForHost = new NavHost();
+	private JFrame frame;
+
+	public void close() {
+		frame.dispose();
+	}
+
+	/**
+	 * Create the application.
+	 */
+
+	 private Controller controller;
+	 private Model model;
+	 private MainModule mainModule;
+	 public GuestAccount(MainModule mainModule, Controller controller, Model model) {
+		initializeGuestAccount();
+		this.model=model;
+		this.mainModule=mainModule;
+		this.controller=controller;
+	 }
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	public void initializeGuestAccount() {
+
+		try {
+			frame = new JFrame();
+			navForHost.addHostNav(frame, mainModule);
+			
+		}catch(Exception e) {
+			System.err.println(e.getMessage());
+		}
+		
+		
+		JPanel registerPanel = new JPanel();
+		registerPanel.setBackground(new Color(204, 255, 255));
+		frame.getContentPane().add(registerPanel, BorderLayout.CENTER);
+		registerPanel.setLayout(null);
+		
+
+		JLabel guestAccountLabel = new JLabel("Guest Account");
+		guestAccountLabel.setFont(new Font("Tahoma", Font.PLAIN, 23));
+		guestAccountLabel.setBounds(222, 53, 183, 57);
+		registerPanel.add(guestAccountLabel);
+		
+		JButton editAccountButton = new JButton("Edit Account");
+		editAccountButton.setBounds(203, 177, 183, 34);
+		registerPanel.add(editAccountButton);
+		
+		JButton provisionalBookingsLabel = new JButton("Provisional Bookings");
+		provisionalBookingsLabel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mainModule.editPropertyState = EDITPROPERTY.EDIT_PROPERTY;
+				MainModule.controller.editPropertyView(0);
+				frame.dispose();
+			}
+		});
+		provisionalBookingsLabel.setBounds(203, 269, 183, 34);
+		registerPanel.add(provisionalBookingsLabel);
+		
+		JButton bookingsButton = new JButton("Bookings List");
+		bookingsButton.setBounds(203, 351, 183, 34);
+		registerPanel.add(bookingsButton);
+		
+		frame.setBounds(100, 100, 600, 700);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
