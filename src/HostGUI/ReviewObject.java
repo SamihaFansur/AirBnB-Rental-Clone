f64c59e8be8c0231654f4a2365bcb815c1afe38b
Euan Goodbrand <70519638+euangoodbrand@users.noreply.github.com>
2021-11-29 14:34:25 +0000

    Correct indent and remove test code ReviewObject
    Correct indent and remove test code ReviewObject

--- src/HostGUI/ReviewObject.java
+++ src/HostGUI/ReviewObject.java
@@ -2,58 +2,57 @@ package HostGUI;
 
 public class ReviewObject {
 
-	    private int review_id;
-	    private int property_id;
-	    private Double accuracy;
-	    private Double location;
-	    private Double valueForMoney;
-	    private Double communication;
-	    private Double cleanliness;
-	    private String description;
+	private int review_id;
+	private int property_id;
+	private Double accuracy;
+	private Double location;
+	private Double valueForMoney;
+	private Double communication;
+	private Double cleanliness;
+	private String description;
 
-	    /**
-	     * @wbp.parser.entryPoint
-	     */
-	    public ReviewObject(int review_id, int property_id, Double accuracy, Double location, Double valueForMoney,Double communication, Double cleanliness, String description) {
-	        this.review_id = review_id;
-	        this.property_id = property_id ;
-	        this.accuracy = accuracy;
-	        this.location = location;
-	        this.valueForMoney = valueForMoney;
-	        this.communication = communication;
-	        this.cleanliness = cleanliness;
-	        this.description = description;
+	public ReviewObject(int review_id, int property_id, Double accuracy, Double location, Double valueForMoney,
+			Double communication, Double cleanliness, String description) {
+		this.review_id = review_id;
+		this.property_id = property_id;
+		this.accuracy = accuracy;
+		this.location = location;
+		this.valueForMoney = valueForMoney;
+		this.communication = communication;
+		this.cleanliness = cleanliness;
+		this.description = description;
+	}
 
-	    }
+	public int getReview_id() {
+		return review_id;
+	}
 
-	    public int getReview_id() {
-	        return review_id;
-	    }
-	    public int getProperty_id() {
-	        return property_id;
-	    }
+	public int getProperty_id() {
+		return property_id;
+	}
 
-	    public Double getAccuracy() {
-	        return accuracy;
-	    }
+	public Double getAccuracy() {
+		return accuracy;
+	}
 
-	    public Double getLocation() {
-	        return location;
-	    }
-	    public Double getValueForMoney() {
-	        return valueForMoney;
-	    }
+	public Double getLocation() {
+		return location;
+	}
 
-	    public Double getCommunication() {
-	        return communication;
-	    }
+	public Double getValueForMoney() {
+		return valueForMoney;
+	}
 
-	    public Double getCleanliness() {
-	        return cleanliness;
-	    }
+	public Double getCommunication() {
+		return communication;
+	}
 
-	    public String getDescription() {
-	        return description;
-	    }
+	public Double getCleanliness() {
+		return cleanliness;
+	}
+
+	public String getDescription() {
+		return description;
+	}
 
 }
