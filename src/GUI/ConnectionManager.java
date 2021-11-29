23acf312d1d080255e73c2839ab70e779ef563f2
Euan Goodbrand <70519638+euangoodbrand@users.noreply.github.com>
2021-11-29 14:47:05 +0000

    Correct indent and remove test contact and connectionManager
    Correct indent and remove test contact and connectionManager

--- src/GUI/ConnectionManager.java
+++ src/GUI/ConnectionManager.java
@@ -6,38 +6,32 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class ConnectionManager {
-    private static String serverName = "jdbc:mysql://stusql.dcs.shef.ac.uk/team018";
-    private static String username = "team018";
-    private static String pwd = "7854a03f";
-    private static Connection connection;
-//    private static String urlString;
+	private static String serverName = "jdbc:mysql://stusql.dcs.shef.ac.uk/team018";
+	private static String username = "team018";
+	private static String pwd = "7854a03f";
+	private static Connection connection;
 
-    public static Connection getConnection() throws SQLException {
-        try {
-            connection = DriverManager.getConnection(serverName, username, pwd);
-            System.out.println("Conn made");
-        }catch(SQLException ex) {
-            System.out.println("failed to make conn");
-            ex.printStackTrace();
-        }finally {
-//            connection.close();
-            System.out.println("conn still open");
-        }
-        return connection;
-    }
+	public static Connection getConnection() throws SQLException {
+		try {
+			connection = DriverManager.getConnection(serverName, username, pwd);
+			System.out.println("Conn made");
+		} catch (SQLException ex) {
+			System.out.println("failed to make conn");
+			ex.printStackTrace();
+		} finally {
+			System.out.println("conn still open");
+		}
+		return connection;
+	}
 
-
-
-
-    public static void deleteProperty(int id){
-        String sql = "DELETE FROM Property WHERE property_id=?";
-        try{
-            PreparedStatement ps = connection.prepareStatement(sql);
-            ps.setInt(1, id);
-
-            ps.executeUpdate();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
+	public static void deleteProperty(int id) {
+		String sql = "DELETE FROM Property WHERE property_id=?";
+		try {
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.setInt(1, id);
+			ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
