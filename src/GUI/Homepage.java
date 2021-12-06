aff58c409689e3025fb63ff20df65be295b9f369
Euan Goodbrand <70519638+euangoodbrand@users.noreply.github.com>
2021-12-06 11:01:26 +0000

    Formatted Homepage
    Formatted Homepage

--- src/GUI/Homepage.java
+++ src/GUI/Homepage.java
@@ -13,6 +13,7 @@ import Controller.Controller;
 import GUI.MainModule.STATE;
 import GUI.MainModule.USER;
 import Model.Model;
+import javax.swing.JTextField;
 
 public class Homepage extends JFrame{
 	/**
@@ -23,9 +24,9 @@ public class Homepage extends JFrame{
 	private MainModule mainModule;
 	private NavEnquirer navBeforeLogin = new NavEnquirer();
 	private JFrame frame ;
+	private JTextField txtHomebreaksPlcManages;
 
 	 public Homepage(MainModule mainModule, Controller controller, Model model) {
-		//initializeHomePage();
 		this.model=model;
 		this.mainModule=mainModule;
 		this.controller=controller;
@@ -35,14 +36,15 @@ public class Homepage extends JFrame{
 	 * Initialize the contents of the frame.
 	 */
 	 public void initializeHomePage() {
+
 		 if(mainModule.userState == USER.ENQUIRER) {
 			 mainModule.currentState = STATE.HOMEPAGE;
 				try {
 					frame = new JFrame();
-					navBeforeLogin.addNavBeforeLogin(frame, mainModule);
 				}catch(Exception e) {
 					System.err.println(e.getMessage());
 				}
+				
 			JPanel homepagePanel = new JPanel();
 			homepagePanel.setBackground(new Color(204, 255, 255));
 			frame.getContentPane().add(homepagePanel, BorderLayout.CENTER);
@@ -53,28 +55,22 @@ public class Homepage extends JFrame{
 			homePageLabel.setBounds(190, -27, 250, 152);
 			homepagePanel.add(homePageLabel);
 			
-			String description = "HomeBreaks Plc manages a collection of small properties for short-term "
-								+ "lease by hosts to renting guests. This software will enable you to sign "
-								+ "up as a either as a host or a guest or both.\nIf you would like to advertise "
-								+ "your properties to be rented please create a host account. If you would like to "
-								+ "rent properties please create a guest account. If you would like to have access to "
-								+ "both advertising and renting properties you can create a joint host and guest account. "
-								+ "\nEach advertised property is available for certain time periods. A property offers a "
-								+ "number of facilities, where each facility is themed after a zone in the property. "
-								+ "The six possible kinds of facility have standard names: sleeping, bathing, kitchen, "
-								+ "living, utility and outdoors. \nIf you'd like to search from available properties use "
-								+ "the 'Search' page to do so. To use the system features please login.";
-	        JTextArea textArea = new JTextArea(description);  
-	        textArea.setLineWrap(true);
-			textArea.setBounds(75,100,430,400);		
-			textArea.setFont(new Font("Arial", Font.PLAIN, 16));	
-			homepagePanel.add(textArea);
+			JTextArea txtHomebreaksPlcManages = new JTextArea("HomeBreaks Plc manages a collection of small properties for short-term  lease by hosts to renting guests. This software will enable you to sign up as a either as a host or a guest or both. If you would like to advertise your properties to be rented please create a host account. If you would like to rent properties please create a guest account. If you would like to have access to both advertising and renting properties you can create a joint host and guest account. Each advertised property is available for certain time periods. A property offers a number of facilities, where each facility is themed after a zone in the property. The six possible kinds of facility have standard names: sleeping, bathing, kitchenliving, utility and outdoors. If you'd like to search from available properties use the 'Search' page to do so. To use the system features please login."); 
+			txtHomebreaksPlcManages.setBounds(67, 119, 461, 390);
+			txtHomebreaksPlcManages.setLineWrap(true);
+			txtHomebreaksPlcManages.setWrapStyleWord(true);
+			
+			txtHomebreaksPlcManages.setFont(new Font("Arial Black", Font.PLAIN, 15));
+			homepagePanel.add(txtHomebreaksPlcManages);
+			
+
 			
 			frame.setBounds(100, 100, 600, 700);
 			frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
 			frame.setLocationRelativeTo(null);
 			frame.setVisible(true);
+
 		 }
 		 else if (mainModule.userState==USER.HOST) {
 		 }
@@ -82,3 +78,4 @@ public class Homepage extends JFrame{
 		 }
 	}
 }
+
