112cd826879392221cb7965af70c1c835ecbd493
Samiha Fansur <samiha.fansur.2002@gmail.com>
2021-12-06 08:02:25 +0000

    checkdriver exists commented

--- src/CheckDriversExist.java
+++ src/CheckDriversExist.java
@@ -1,9 +1,11 @@
 import java.sql.*;
 import java.util.*;
+
+/**Use this file to check if your driver exists so the system can function as intended*/
+
 public class CheckDriversExist {
 
 	public static void main(String[] args) throws Exception{
-		// TODO Auto-generated method stub
 		System.out.println("\nDrivers loaded as properties: ");
 		System.out.println(System.getProperty("jdbc.drivers"));
 		System.out.println("\nDrivers loaded by DriverManager: ");
